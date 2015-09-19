4dbe96003739385ff16785a88e5fe970ef3be9c5
Eric Olson <eric.rob.olson@gmail.com>
2015-09-19 11:35:36 -0700

    Changed the database name, and added a table for Combos. Added in a function that adds a combo + card to the database.

--- database.py
+++ database.py
@@ -7,20 +7,27 @@ import json
 
 # if the database doesn't exist, run this function to create it.
 def f_init_database():
-  db = sqlite3.connect("AllCards.sqlite3")
+  db = sqlite3.connect("AllThings.sqlite3")
   cursor = db.cursor()
-  # primary keys, and foreign keys.
-  # table for cards, colors and combos
+  # table for cards
   cursor.execute('''CREATE TABLE AllCards
       (name TEXT PRIMARY KEY UNIQUE NOT NULL,
       cmc INTEGER,
       text TEXT)''' )
 
+  # table for colors
   cursor.execute("""CREATE TABLE AllColors
-     (color TEXT NOT NULL,
-     name TEXT NOT NULL, 
-     FOREIGN KEY (name) REFERENCES AllCards(name),
-     PRIMARY KEY (color, name))""")
+      (color TEXT NOT NULL,
+      name TEXT NOT NULL, 
+      FOREIGN KEY (name) REFERENCES AllCards(name),
+      PRIMARY KEY (color, name))""")
+
+  # table for combos
+  cursor.execute("""CREATE TABLE AllCombos
+      (name TEXT NOT NULL,
+      card TEXT NOT NULL,
+      FOREIGN KEY (card) REFERENCES AllCards(name),
+      PRIMARY KEY (name, card))""")
      
   db.commit()
   db.close()
@@ -29,7 +36,7 @@ def f_init_database():
 # run this function to create/update card entries based off of AllCards.json
 def f_update_AllCards():
   # load our database, and our json file
-  db = sqlite3.connect("AllCards.sqlite3")
+  db = sqlite3.connect("AllThings.sqlite3")
   with open('AllCards.json') as data_file:
     # for each subfield in Json:
     data = json.load(data_file)
@@ -73,5 +80,25 @@ def f_update_AllCards():
   db.close()
 
 
-# f_init_database()
-f_update_AllCards()
+# add a combo into the database
+def f_add_Combo(combo_name, card_name):
+  db = sqlite3.connect("AllThings.sqlite3")
+  cursor = db.cursor()
+
+  # enter the combo + card into the database
+  try:
+    cursor.execute("INSERT INTO AllCombos VALUES (?, ?)", 
+        (combo_name, card_name))
+  except:
+    print("The combo " + combo_name + " with card " +
+        card_name + " already exists.")
+  
+  db.commit()
+  db.close()
+
+
+
+#f_add_Combo("COMBO123", "Mountain")
+#f_add_Combo("COMBO123", "Krenko")
+#f_init_database()
+#f_update_AllCards()
